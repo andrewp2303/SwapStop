30c2ec1eb0fbf8de90c973f01b0f1e349977db4d
Eyobdd <eyobdavidoff@college.harvard.edu>
2021-12-06 02:16:42 -0500

    small changes

--- swapproj/application.py
+++ swapproj/application.py
@@ -30,4 +30,22 @@ def createlisting():
 @bp.route("/viewitem", methods=["GET","POST"])
 def viewitem():
     if request.method == "GET":
-        return render_template("viewitem.html")
+        # item = db_session.execute("SELECT * FROM items WHERE id = :id",{'id':session["item_id"]}).first()
+        item = {'description': "coolapp", 'sold':"No",'timestamp':"22/02/2021"}
+        return render_template("viewitem.html", item=item)
+    else:
+        if request.form.get("proposetrade"):
+            # TODO: Proposes trade
+            return "Not implemented"
+        elif request.form.get("contact"):
+            # TODO: Displays contact information
+            return "Not implemented"
+        else:
+
+            return redirect("/viewitems")
+
+
+# @bp.route("/contact", methods=["GET"])
+# def contact():
+#     ownerinfo = db_session.execute("SELECT * FROM users WHERE id IN (SELECT user_id FROM items WHERE item.id = :id)",{'id':session["item_id"]})
+#     return render_template("viewcontactinfo.html", info=ownerinfo)
