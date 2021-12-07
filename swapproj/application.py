a55e7d2339e0a8222883e0014e36f0aa06f4f83e
Eyobdd <eyobdavidoff@college.harvard.edu>
2021-12-07 11:52:40 -0500

    Comments on application.py

--- swapproj/application.py
+++ swapproj/application.py
@@ -17,44 +17,63 @@ bp = Blueprint('application', __name__, url_prefix='/', static_url_path='')
 extensions = ['.jpg', '.png', '.gif', '.jpeg']
 upload_path = 'swapproj/static/images'
 
+# Route /myitems displays user listed/trade items
+# GET: Retrieves and displays all items that are associated with the logged-in user
+# POST: Displays information and messages pertaining to a specific item
 @bp.route("/myitems", methods=["GET", "POST"])
 def myitems():
     if request.method == "GET":
+
+        # Retrieves user items
         items = db_session.query(Item).filter_by(user_id = session['user_id']).all()
+        
+        # Renders template that displays owned/previously owned items
         return render_template("myitems.html", items=items)
     else:
+        # Retrieves messages and information on a specific item
         itemid = request.form.get("itemid")
         item = db_session.query(Item).filter_by(id = itemid).first()
         # messages = db_session.query(Message).filter_by(item_id = itemid).all()
         # TODO: Fix displayed time
         messages = db_session.execute("SELECT messages.text, messages.timestamp, users.username, users.email, users.first_name, users.last_name FROM users INNER JOIN messages ON users.id = messages.sender_id WHERE messages.item_id=:id",{'id':itemid}).all()
+        
+        # Displays items and messages using myitem.html 
         return render_template("myitem.html",item=item, messages=messages)
 
+
+# Route /myitem handles POST requests previously rendered myitem.html
+# POST: Edit - loads a page that allows the user to edit selected listing
+# POST: Cancel - Allows user to navigate back to their /myitems page
 @bp.route("/myitem", methods=["POST"])
 def myitem():
     if request.form.get("edit"):
+
+        # Retrieves item information
         itemid = request.form.get("edit")
         item = db_session.query(Item).filter_by(id=itemid).first()
+
+        # Displays UI to edit their listing
         return render_template("edit.html", item=item)
     else:
+
+        # Redirects user back to their owned items page
         return redirect("/myitems")
 
 
-
+# Route /edit only handles POST requests, either delete or update a listing
+# POST: Update - Retrieves updated forms from edit.html and updates item in the databases
+# POST: Delete - Deletes selected item and corresponding messages from database
 @bp.route("/edit", methods=["POST"])
 def edit():
     if request.form.get("update"):
+        # Retrives changed listings from form
         itemid = request.form.get("update")
         citem = db_session.query(Item).filter_by(id=itemid).first()
         name = request.form.get("title")
         description = request.form.get("description")
         sold = True if request.form.get("sold") == "True" else False
-        print("\nITEMID:")
-        print(itemid)
-        print(citem.id)
-        print(sold)
-        print(citem.sold==False)
-        print(sold==False)
+        
+        # initalizes the "updated item"
         item = {
             'name':name,
             'description':description,
@@ -63,47 +82,69 @@ def edit():
             'sold':sold,
             'user_id':citem.user_id
         }
+
+        # Updates listing in database
         db_session.query(Item).filter_by(id=itemid).update(item)
         db_session.commit()
+
+        # Returns the user to up
         flash("Listing Updated")
         return redirect("/myitems")
     else:
+        # Deletes messages and items database
         db_session.execute("DELETE FROM messages WHERE item_id =:id",{'id':request.form.get("delete")})
         db_session.execute("DELETE FROM items WHERE id =:id",{'id':request.form.get("delete")})
         db_session.commit()
+        
+        # Notifies user that there item was succesfully removed
         flash("Listing Deleted.")
         return redirect("/myitems")
 
-
+# Route /viewitems handles both GET and POST request
+# GET: Retrieves and displays from database every item that available and not owned by the user
+# POST: Renders a specific items UI presenting with options if they want to contact the owner of the item
 @bp.route("/viewitems", methods=["GET", "POST"])
 def viewitems():
     if request.method == "GET":
+        
+        # Retrieve items that are to be displayed
         items = db_session.query(Item).filter_by(sold = False).filter(Item.user_id!=session["user_id"]).all()
+        
+        # Displays items
         return render_template("viewitems.html", items=items)
     else:
+
+        # Retrieves selected item info
         itemid = request.form.get("itemid")
         item = db_session.query(Item).filter_by(id = itemid).first()
+
+        # Displays select item
         return render_template("viewitem.html",item=item)
 
 
+# Route /viewitem takes only POST requests
+# POST: Contact -- allows the user to make an inquery about an item
+# POST: Back -- allows the user to navigate back a page
 @bp.route("/viewitem", methods=["POST"])
 def viewitem():
     if request.form.get("contact"):
         itemid = request.form.get("contact")
-        #item = db_session.query(Item).filter_by(id=item_id).first()
         return render_template("contact.html", itemid=itemid)
     else:
         return redirect("/viewitems")
 
+# Route /contact takes only POST requests and allows user to send messages
+# POST: Message -- adds a message to the database and prompts user when the message is sent
 @bp.route("/contact", methods=["POST"])
 def contact():
 
-
+    # Checks if the user provided a message
     if not request.form.get("description"):
         flash("Please provide a description!")
         itemid = request.form.get("itemid")
         return render_template("contact.html", itemid=itemid)
     else:
+        # Initializes the new message
         itemid = request.form.get("itemid")
         item = db_session.query(Item).filter_by(id=itemid).first()
         message = Message(
@@ -113,25 +154,33 @@ def contact():
             text = request.form.get("description"),
             item_id = item.id
         )
+        # Inserts into database
         db_session.add(message)
         db_session.commit()
+
+        # Prompts user and redirects to "marketplace"
         flash("Message sent!")
         return redirect("/")
 
-
+# Route /listitem takes both GET and POST requests
+# GET: Displays form that allows user to and a listing
+# POST: Creates Listing -- Retrieves information from forms and adds it to the database
 @bp.route("/listitem", methods=["GET", "POST"])
 def listitem():
     if request.method == "GET":
         return render_template("listitem.html")
     else:
+        # Retrieves information from forms
         title = request.form.get("title")
         description = request.form.get("description")
         img = request.files['file']
         
+        # Checks if all fields are filled
         if not(title or description or img):
             flash("Please provide title, description, and image")
             return redirect("/listitem")
-
+        
+        # Adds img to database or displays error
         filename = secure_filename(img.filename)
         if filename != '':
             file_ext = os.path.splitext(filename)[1]
@@ -143,6 +192,7 @@ def listitem():
             flash("Please provide an image")
             return redirect("/listitem")
 
+        # Initializes item
         item = Item(
             name=title,
             description=description,
@@ -151,12 +201,14 @@ def listitem():
             sold=False,
             user_id=session["user_id"]
         )
+
+        # Adds initialized item to datadbase
         db_session.add(item)
         db_session.commit()
 
+        # Prompts user and Redirects them to /myitems
         flash("New listing created!")
-        # Future TODO: redirect to /myitems
-        return redirect("/")
+        return redirect("/myitems")
 
 # Finds the filename to display an image
 @bp.route('/static/<filename>', methods = ["GET"])
